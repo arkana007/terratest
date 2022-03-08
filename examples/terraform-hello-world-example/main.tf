525118c2f44bdfa89b6d62a189d9b47578dde11e
arkana007 <motox7381@gmail.com>
2022-03-08 11:45:17 -0600

    Exerciese FirstExercise Added

--- examples/terraform-hello-world-example/main.tf
+++ examples/terraform-hello-world-example/main.tf
@@ -4,7 +4,6 @@ terraform {
   # forwards compatible with 0.13.x code.
   required_version = ">= 0.12.26"
 }
-
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
   value = "Hello, World!"
